058353e53be83604a0e8cb4529a2c8353b168ad0
gregorian <gregorian_rants@hotmail.com>
2023-09-14 19:27:17 +0100

    having problems with connecting to wsl over zeromq commiting prior to switching to windows

---
+++ desktop/vision/floor-detector.py
@@ -0,0 +1,24 @@
+import sys
+import zmq
+import cv2
+import numpy as np
+
+
+#  Socket to talk to server
+context = zmq.Context()
+socket = context.socket(zmq.SUB)
+socket.connect("tcp://192.168.178.52:5556")
+socket.setsockopt(zmq.SUBSCRIBE, b"")
+socket.setsockopt(zmq.CONFLATE, 1)
+
+pub_socket = context.socket(zmq.PUB)
+pub_socket.bind("tcp://*:5559")
+
+
+while True:
+    received_bytes = socket.recv()
+    np_array = np.frombuffer(received_bytes, dtype=np.uint8)
+    image = cv2.imdecode(np_array, 1)
+    pub_socket.send_string("hello")
+    cv2.imshow("image", image)
+    cv2.waitKey(1)
