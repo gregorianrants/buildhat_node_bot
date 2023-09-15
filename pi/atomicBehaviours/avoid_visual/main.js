72cd886408e5c8d948cd65d9d9431b06d3a8849a
gregorianrants <gregorian_rants@hotmail.com>
2023-09-15 14:01:36 +0100

    got avoid visual recieving messages from desktop now that it is running on windows, shouldnt have modified master small change so will just leave it

--- pi/atomicBehaviours/avoid_visual/main.js
+++ pi/atomicBehaviours/avoid_visual/main.js
@@ -1,30 +1,14 @@
-// zmq = require('zeromq')
-
-// async function init(){
-//     socket = new zmq.Subscriber()
-//     socket.connect('tcp://172.31.21.98:5559')
-//     socket.subscribe('h')
-
-//     for await(const [msg] of socket){
-//         console.log(msg)
-//     }
-// }
-
-
-
-// init()
-
 const zmq = require("zeromq")
 
 async function run() {
   const sock = new zmq.Subscriber
 
   sock.connect("tcp://192.168.178.47:5559")
-  sock.subscribe("kitty cats")
-  console.log("Subscriber connected to port 3000")
+  sock.subscribe("")
 
-  for await (const [topic, msg] of sock) {
-    console.log("received a message related to:", topic, "containing message:", msg)
+
+  for await (const [msg] of sock) {
+    console.log( msg)
   }
 }
 
