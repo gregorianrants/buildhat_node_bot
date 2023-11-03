2082232813b1532761b151a17545b5f7bc47f058
gregorianrants <gregorian_rants@hotmail.com>
2023-11-03 10:33:56 +0000

    modified readme

--- README.md
+++ README.md
@@ -1,47 +1,11 @@
-# buildhat-node-bot
-This is the codebase for a differential drive robot that uses the raspberrypi build hat to control the motors.  The buildhat is controled via its serial protocol using nodejs rather than the python library.
+# Raspberry Pi robot 
 
-![image of the robot](./robot.jpg)
+Obstacle avoiding raspberry pi robot, with distributed software architecture, at the moment this readme just gives an overview of what the hardware and does.  Soon more information will be added on both how the software works and how it was designed. 
 
-# important notes
-This codebase is a first proof of concept that i have published for anyone that wants to follow what i am doing.
+There are parts of this project that aren’t completed this will probably always be the case.  This is a passion project, it is something I plan to continuously experiment with refactor and add to.  Although there is much in an uncompleted state there is also much that can run and works brilliantly. 
 
-The buildhat is only used for motors, and the code to use it will be developed for this purpose only.
+This library works in conjunction with 3 other libraries. 
 
-If you want to use it you probably need to figure out what the code does and modify it.
+## Navigating a complex environment
 
-When the build hat turns on it does not have the firmaware on it so you need to run the npm-script load-firmaware (requires python) to load this before running other scripts.
-
-There are paramters specific to my robot in this code, they are not all grouped in an obvious place (yet).
-this is code that is being experimented with and developed at the same time.
-
-the paramters of the pid control of speed may need to be tuned for your robot.  i will at some point add some graphing features and the ability to store and compare statistics for different pid values to make this easier.
-
-it uses the serialprotocol for the buildhat via nodejs rather than the python library
-
-
-# how can i use this
-there are scripts in package.json that runs what it can do so far.
-
-## what can it do so far
-- contorl motors 
-- emit motor tacho info as events
-- contorl speed of motors using pid
-- control translational and rotational speed of robot.
-- read distance sensors (using pigpio via nodejs - not via the buildhat)
-
-# why am i building this
-
-1. the speed control via the python library is not suitable for a wheeled robot, there is a delay between the motors starting which just makes it spin round.
-2. the python library uses threads to achieve concurrency, i wanted to use event loop style asynchronous programming.
-
-# why i chose nodejs
-
-I started with using asyncio in python but i am more familiar with nodejs and found it much easier to achieve what i wanted with it.  
-In particular several parts of my codebase will have to react to the same events, the nodejs event emitter makes this easy.
-I found that i could do some things with node in one declarative line that took many hard to follow lines of garbled code in python.
-e.g. reading from the serial port.
-
-# where am i going with this
-I am actively developing this and it will change frequently
-I will add more info here soon about what the code does and where i am going with it.
+![robot](https://drive.google.com/file/d/1KNquuaBwte_ihSQTIUiOxNgJBPI0xS6c/view?usp=drive_link)
--- vision/ws-server-nodejs/main.js
+++ vision/ws-server-nodejs/main.js
@@ -1,4 +1,58 @@
 //192.168.178.52:3000
+// const { WebSocketServer } = require("ws");
+// const { spawn } = require("child_process");
+// const { to } = require("mathjs");
+// const os = require("os");
+// console.log(os.endianness());
+
+// const { ReadlineParser } = require("@serialport/parser-readline");
+
+// const wss = new WebSocketServer({ port: 3000 });
+
+// wss.on("connection", function connection(ws) {
+//   const ls = spawn("libcamera-vid", [
+//     "-t",
+//     "0",
+//     "--codec",
+//     "mjpeg",
+//     "--segment",
+//     "1",
+//     "-o",
+//     "-",
+//     "--width",
+//     "640",
+//     "--height",
+//     "480",
+//     "--framerate",
+//     "20"
+//   ]);
+
+//   ls.stderr.on("error", (error) => {
+//     console.error("child process error", error);
+//   });
+
+//   ls.on("close", (code) => {
+//     console.log(`child process exited with code ${code}`);
+//   });
+
+//   ls.stdout.on("data", (data) => {
+//       ws.send(data)
+//   });
+
+//   ws.on("error", console.error);
+
+//   ws.on("message", function message(event) {
+//       console.log("received:", event.toString());
+//   });
+
+//   ws.on("close", () => {
+//     ls.kill("SIGINT");
+//     console.log("web socket closed");
+//   });
+// });
+
+// process.on("uncaughtException", (err) => console.error('there was an error:',err));
+
 const { WebSocketServer } = require("ws");
 const { spawn } = require("child_process");
 const { to } = require("mathjs");
@@ -7,48 +61,66 @@ console.log(os.endianness());
 
 const { ReadlineParser } = require("@serialport/parser-readline");
 
-const wss = new WebSocketServer({ port: 3000 });
+const ls = spawn("libcamera-vid", [
+  "-t",
+  "0",
+  "--codec",
+  "mjpeg",
+  "--segment",
+  "1",
+  "-o",
+  "-",
+  "--width",
+  "640",
+  "--height",
+  "480",
+  "--framerate",
+  "20",
+]);
 
-wss.on("connection", function connection(ws) {
-  const ls = spawn("libcamera-vid", [
-    "-t",
-    "0",
-    "--codec",
-    "mjpeg",
-    "--segment",
-    "1",
-    "-o",
-    "-",
-    "--width",
-    "640",
-    "--height",
-    "480",
-    "--framerate",
-    "20"
-  ]);
-
-  ls.stderr.on("error", (error) => {
-    console.error("child process error", error);
-  });
-
-  ls.on("close", (code) => {
-    console.log(`child process exited with code ${code}`);
-  });
-
-  ls.stdout.on("data", (data) => {
-      ws.send(data)
-  });
-
-  ws.on("error", console.error);
-
-  ws.on("message", function message(event) {
-      console.log("received:", event.toString());
-  });
-
-  ws.on("close", () => {
-    ls.kill("SIGINT");
-    console.log("web socket closed");
-  });
+ls.stderr.on("error", (error) => {
+  console.error("child process error", error);
 });
 
-process.on("uncaughtException", (err) => console.error('there was an error:',err));
+ls.on("close", (code) => {
+  console.log(`child process exited with code ${code}`);
+});
+
+// ls.stdout.on("data", (data) => {
+//   console.log(data);
+// });
+
+let buffer = Buffer.from("ffd9", "hex");
+const stop = new Uint16Array(
+  buffer.buffer,
+  buffer.byteOffset,
+  buffer.length / 2
+);
+
+console.log(stop);
+console.log(stop.indexOf(255, 217));
+
+ls.stdout.on("readable", () => {
+  let chunk;
+  while ((chunk = ls.stdout.read()) !== null) {
+    let chunk16 = new Uint16Array(
+      chunk.buffer,
+      chunk.byteOffset,
+      chunk.length / 2
+    );
+    //console.log(chunk16);
+    //console.log(stop);
+    let indexOfStop = chunk16.indexOf(stop[0]);
+    if (indexOfStop !== -1) {
+      console.log(indexOfStop);
+      console.log(chunk16.length);
+      console.log("-----------");
+    } else {
+      console.log("-----");
+    }
+  }
+});
+
+process.on("uncaughtException", (err) =>
+  console.error("there was an error:", err)
+);
